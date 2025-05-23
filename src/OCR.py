7a87619736b7262b8e273275edc5c78e467191a7
Ironside <93154332+motakosay@users.noreply.github.com>
2025-05-23 22:15:13 +0300

    Update OCR.py

--- src/OCR.py
+++ src/OCR.py
@@ -57,7 +57,7 @@ def run(image_path):
 
     exc_time = after-before
     # Create file with the same name of the image
-    img_name = image_path.split('\\')[1].split('.')[0]
+    img_name = os.path.splitext(os.path.basename(image_path))[0]
 
     with open(f'output/text/{img_name}.txt', 'w', encoding='utf8') as fo:
         fo.writelines(predicted_text)
